a99fc3eb2e0711ecf13a15b2e8027172001ce7b3
shmuly levitin <shmuly11@gmail.com>
2020-11-25 14:33:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,25 +20,25 @@ end
 
 
 def get_english_meaning(file, emoticon)
- list1 =  load_library(file)
+# list1 =  load_library(file)
  
- list1.each do |name, emoticons|
+ #list1.each do |name, emoticons|
    
-  #if  emoticons.any? { |lang, symbol| symbol == emoticon }
-    emoticons.each do |lang, symbol|
-      symbol.each do 
-        if symbol == emoticon
+  ##if  emoticons.any? { |lang, symbol| symbol == emoticon }
+   # emoticons.each do |lang, symbol|
+      #symbol.each do 
+     #   if symbol == emoticon
      #binding.pry 
-     # if emoticon == symbol
+    # # if emoticon == symbol
       # binding.pry
-        return name
+     #   return name
        # binding.pry
-     else
-       return "Sorry, that emoticon was not found"
-     end
-   end
+    # else
+     #  return "Sorry, that emoticon was not found"
+    # end
+   #end
   
-    end
+    #end
     tempHash = load_library(file)
   englishMeaning = (tempHash.select { |k,v| v[:japanese] == emoticon}).keys.join
   tempHash.include?(englishMeaning) ? englishMeaning : "Sorry, that emoticon was not found"
